1473f85c549eedb15869ff3549a6c7a4c5c878bf
Louie Christie <6807448+louiechristie@users.noreply.github.com>
2019-01-12 07:03:42 +0000

    Remove redundant code.

--- src/features/insights/containers/InsightsContainer.js
+++ src/features/insights/containers/InsightsContainer.js
@@ -24,13 +24,6 @@ class InsightsContainer extends React.Component {
   }
 }
 
-<NavigationEvents
-  onWillFocus={payload => console.log('will focus', payload)}
-  onDidFocus={payload => console.log('did focus', payload)}
-  onWillBlur={payload => console.log('will blur', payload)}
-  onDidBlur={payload => console.log('did blur', payload)}
-/>;
-
 const mapStateToProps = state => {
   return {
     checkins: state.checkins,
